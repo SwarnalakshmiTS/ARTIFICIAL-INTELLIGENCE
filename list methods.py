53d99f97369629ac7f6e390911500c852f14078c
SwarnalakshmiTS <112744358+SwarnalakshmiTS@users.noreply.github.com>
2023-02-01 23:57:09 +0530

    Add files via upload

---
+++ list methods.py
@@ -0,0 +1,11 @@
+
+currencies = ['Dollar', 'Euro', 'Pound']
+currencies2=['USA','AFRICA','ANTARTICA']
+currencies.append('Yen')
+print("list after append(): " ,currencies)
+currencies2.extend(currencies)
+print("list after extend(): " ,currencies2)
+currencies2.remove('AFRICA')
+print("list after remove(): " ,currencies2)
+currencies2.insert(3,'SWARNALAKSHMI')
+print("list after insert(): ",currencies2)
---
+++ list operations.py
@@ -0,0 +1,15 @@
+my_list = ['p','r','o','b','e']
+print(my_list[0])
+print("length",my_list,":",len(my_list))
+print(my_list[2])
+print(my_list[4])
+n_list=[[1,3,5,7],[2,4,6,8,10]]
+print(n_list[1][3])
+n_list2=["HAPPY",[2,0,1,5]]
+print(n_list2[0][1])
+print(n_list2[1][3])
+concat1=[1,3,5]
+concat2=[7,8,9]
+print("concatention:",concat2,":",concat1+concat2)
+repetion_string="hello"
+print("repetition:",repetion_string,repetion_string*4)
